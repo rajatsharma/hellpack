1de2d055943265b8eda50e8c24c9587ce5165840
Rajat Sharma <rajatsharma0720@gmail.com>
2018-06-06 04:18:55 +0530

    Added express-helpers

--- packages/eslint-config-hellpack/index.js
+++ packages/eslint-config-hellpack/index.js
@@ -1,13 +1,15 @@
+const excludedImports = ['express-helpers'];
 module.exports = {
-  'parser': 'babel-eslint',
-  'extends': require.resolve('eslint-config-airbnb-base'),
-  'plugins': [
-    'import',
-    'flowtype',
-    'flowtype-errors'
-  ],
+  parser: 'babel-eslint',
+  extends: require.resolve('eslint-config-airbnb-base'),
+  plugins: ['import', 'flowtype', 'flowtype-errors'],
   rules: {
+    'import/no-unresolved': ['error', { ignore: excludedImports }],
+    'import/extensions': ['error', 'never', { ignore: excludedImports }],
     'flowtype-errors/show-errors': 'error',
-    'no-unused-vars': ['error', { 'argsIgnorePattern': '^_', 'varsIgnorePattern': '^_' }],
-  }
-}
+    'no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+  },
+};
