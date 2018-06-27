9c5297fc2b20a96e0d688665c849744696889ae1
Rajat Sharma <rajatsharma0720@gmail.com>
2018-06-27 13:50:27 +0530

    Deleted example

--- packages/example/index.js
+++
@@ -1,6 +0,0 @@
-/**
- * Entry Script
- */
-const polyfill = require('@hellpack/webpack-config/lib/polyfill');
-
-polyfill({ dev: './src/server', prod: './dist/server.bundle' });
--- packages/example/scripts/build.js
+++
@@ -1,4 +0,0 @@
-const nodeAlphaBuilder = require('@hellpack/webpack-config/lib');
-const webpackConfig = require('../paths');
-
-nodeAlphaBuilder(webpackConfig).then(_ => console.log('Done')).catch(e => console.log(e));
