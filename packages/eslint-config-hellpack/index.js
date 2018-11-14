131520df7370f142193a7dc3f822bc2671324f1a
Rajat Sharma <rajatsharma45@users.noreply.github.com>
2018-11-14 14:22:41 +0530

    Fixed config

--- packages/eslint-config-hellpack/index.js
+++ packages/eslint-config-hellpack/index.js
@@ -1,22 +1,27 @@
 const excludedImports = ['express-helpers'];
 const globals = ['fetch', 'Transformer', 'System'];
 
-const config = {
+const config = rules => ({
   parser: 'babel-eslint',
-  extends: [require.resolve('eslint-config-airbnb-base'), require.resolve('eslint-config-prettier')],
+  extends: [
+    require.resolve('eslint-config-airbnb-base'),
+    require.resolve('eslint-config-prettier'),
+  ],
   plugins: ['import'],
   globals: globals.reduce(
     (acc, x) => Object.assign({}, acc, { [x]: true }),
     {},
   ),
   rules: {
+    quotes: ['error', 'single'],
     'import/no-unresolved': ['error', { ignore: excludedImports }],
     'import/extensions': ['error', 'never', { ignore: excludedImports }],
     'no-unused-vars': [
       'error',
       { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
     ],
+    ...rules,
   },
-};
+});
 
 module.exports = config;
