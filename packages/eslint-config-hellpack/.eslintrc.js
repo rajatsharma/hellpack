59ce735882b2526b5893500fa7b75baed4a5d499
Rajat Sharma <lunasunkaiser@gmail.com>
2019-02-21 11:21:34 +0530

    WIP plugins

--- packages/eslint-config-hellpack/.eslintrc.js
+++ packages/eslint-config-hellpack/.eslintrc.js
@@ -1,7 +1,26 @@
+const path = require('path');
+const readPkgUp = require('read-pkg-up');
+const fs = require('fs');
+
+const {
+  pkg,
+  path: pkgPath
+} = readPkgUp.sync({
+  cwd: fs.realpathSync(process.cwd())
+});
+
+const appDirectory = path.dirname(pkgPath);
+const packageJson = require(path.resolve(appDirectory, 'package.json'));
+
+const reactInPackages = Object.keys(
+  packageJson.dependencies || {},
+).includes('react');
+
+
 module.exports = {
   parser: 'babel-eslint',
   extends: [
-    require.resolve('eslint-config-airbnb'),
+    require.resolve(reactInPackages ? 'eslint-config-airbnb': 'eslint-config-airbnb-base'),
     require.resolve('eslint-config-prettier'),
     require.resolve('eslint-config-prettier/react'),
   ],
