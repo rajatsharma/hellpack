147ae13ff6b108173c7a55a30b5588afab6a454e
Rajat Sharma <rajatsharma0720@gmail.com>
2019-02-21 12:30:38 +0530

    Added logs and React based installation of plugins

--- packages/eslint-config-hellpack/.eslintrc.js
+++ packages/eslint-config-hellpack/.eslintrc.js
@@ -2,29 +2,28 @@ const path = require('path');
 const readPkgUp = require('read-pkg-up');
 const fs = require('fs');
 
-const {
-  pkg,
-  path: pkgPath
-} = readPkgUp.sync({
-  cwd: fs.realpathSync(process.cwd())
+const { pkg, path: pkgPath } = readPkgUp.sync({
+  cwd: fs.realpathSync(process.cwd()),
 });
 
 const appDirectory = path.dirname(pkgPath);
 const packageJson = require(path.resolve(appDirectory, 'package.json'));
 
-const reactInPackages = Object.keys(
-  packageJson.dependencies || {},
-).includes('react');
-
+const reactInPackages = Object.keys(packageJson.dependencies || {}).includes(
+  'react',
+);
 
 module.exports = {
   parser: 'babel-eslint',
   extends: [
-    require.resolve(reactInPackages ? 'eslint-config-airbnb': 'eslint-config-airbnb-base'),
+    require.resolve(
+      reactInPackages ? 'eslint-config-airbnb' : 'eslint-config-airbnb-base',
+    ),
     require.resolve('eslint-config-prettier'),
     require.resolve('eslint-config-prettier/react'),
   ],
   rules: {
+    'prettier/prettier': 'error',
     'no-unused-vars': [
       'error',
       { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
