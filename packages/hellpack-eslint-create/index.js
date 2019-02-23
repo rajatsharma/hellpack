8a7baf5acf17fc842343a160b4197de64e25f4be
Rajat Sharma <lunasunkaiser@gmail.com>
2019-02-24 04:44:21 +0530

    Hellpack eslint create

---
+++ packages/hellpack-eslint-create/index.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+const path = require('path');
+const { spawnSync } = require('child_process');
+const fs = require('fs');
+
+/* eslint-disable import/no-dynamic-require */
+const packageContents = require(path.resolve(process.cwd(), 'package.json'));
+
+const reactInPackages = Object.keys(
+  packageContents.dependencies || {},
+).includes('react');
+
+const core = ['eslint', 'prettier'];
+
+const config = '@hellpack/eslint-config-hellpack';
+
+const plugins = ['eslint-plugin-prettier', 'eslint-plugin-import'].concat(
+  reactInPackages ? ['eslint-plugin-react', 'eslint-plugin-jsx-a11y'] : [],
+);
+
+const installEslintPlugins = spawnSync(
+  'yarn',
+  ['add', config, ...plugins, ...core, '-D'],
+  {
+    env: process.env,
+    stdio: 'inherit',
+  },
+);
+
+if (installEslintPlugins.error || installEslintPlugins.status !== 0) {
+  console.log(
+    'Eslint Plugins Installation failed, more:',
+    installEslintPlugins.error,
+  );
+
+  console.log(
+    'Please install yourself:\n',
+    `yarn add ${core
+      .concat(plugins)
+      .concat(config)
+      .join(' ')} -D`,
+  );
+}
+const eslint = {
+  extends: config,
+  plugins,
+};
+
+const prettier = {
+  printWidth: 80,
+  singleQuote: true,
+  trailingComma: 'all',
+};
+
+fs.writeFileSync(
+  path.resolve(process.cwd(), '.eslintrc'),
+  JSON.stringify(eslint, null, 2),
+);
+
+fs.writeFileSync(
+  path.resolve(process.cwd(), '.prettierrc'),
+  JSON.stringify(prettier, null, 2),
+);
---
+++ packages/hellpack-eslint-create/readme.md
@@ -0,0 +1,22 @@
+## hellpack-eslint-create
+
+_Latest Prettier and Eslint config for your projects_
+
+Ultracommon eslint config used in my projects, automatically installs `eslint`, `prettier` or `babel-eslint`.
+
+#### Usage
+
+```bash
+$ npx hellpack-eslint-create
+```
+
+### Uses
+
+- `babel-eslint`
+- `eslint`
+- `eslint-config-airbnb`
+- `eslint-config-prettier`
+- `eslint-plugin-import`
+- `eslint-plugin-jsx-a11y`
+- `eslint-plugin-prettier`
+- `eslint-plugin-react`
