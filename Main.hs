fe703b81e385ac276365dbf5e9dc3cacfdf5fad8
Rajat Sharma <lunasunkaiser@gmail.com>
2025-01-19 05:34:27 +0530

    add options

--- Main.hs
+++ Main.hs
@@ -1,12 +1,16 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Main where
 
+import Control.Monad (when)
 import Data.FileEmbed (embedStringFile)
 import System.Directory (getCurrentDirectory)
 import System.FilePath (takeBaseName)
 import System.Process (runCommand)
 import Text.Printf (printf)
+import Turtle (Parser, options, switch)
 
 packageJson :: String
 packageJson = $(embedStringFile "package.json")
@@ -14,11 +18,24 @@ packageJson = $(embedStringFile "package.json")
 tsconfigJson :: String
 tsconfigJson = $(embedStringFile "tsconfig.json")
 
+parser :: Parser (Bool, Bool)
+parser =
+  (,)
+    <$> switch "with-db" 'd' "Specify to install database packages"
+    <*> switch "with-express" 'e' "Specify to install express packages"
+
 main :: IO ()
 main = do
+  (withDb, withExpress) <- options "Hellpack" parser
   currectDirectory <- getCurrentDirectory
   let directoryName = takeBaseName currectDirectory
   writeFile "package.json" $ printf packageJson directoryName
   writeFile "tsconfig.json" tsconfigJson
   runCommand "pnpm i -D prettier @types/node typescript tsx"
-  pure ()
+  when withDb do
+    runCommand "pnpm i slonik"
+    runCommand "db-up"
+    pure ()
+  when withExpress do
+    runCommand "pnpm i express"
+    pure ()
